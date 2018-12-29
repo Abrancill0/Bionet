b660073adb022adc58c46e05e2b0d515e7e5f90e
José Luis <joseluismartinezdominguez16@gmail.com>
2018-12-28 18:18:19 -0600

    Pantallas Clientes
    Se implementaron las pantallas de clientes, ahora solo falta implementar las ventanas emergentes tanto de la preconfiguración y de clientes

--- app/src/main/java/com/example/jl/bionet/Fragment_clientes.java
+++ app/src/main/java/com/example/jl/bionet/Fragment_clientes.java
@@ -1,19 +1,49 @@
 package com.example.jl.bionet;
 
+
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import de.codecrafters.tableview.TableView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
 public class Fragment_clientes extends Fragment {
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_clientes,container,false);
-    }
-}
 
+    static String[][] clientes={
+            {"1","Abraham Moreno","a.moreno@Danthop","4446754321","Hace 20 días","$200.00","4555322"},
+            {"2","Oscar Antonio","oscarsl7@Danthop","4444654321","Hace 13 días","$100.00","4555323"},
+    };
+
+
+    public Fragment_clientes() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_clientes,container, false);
+        Button btn_crear_cliente = (Button) v.findViewById(R.id.btn_crear_cliente);
+        btn_crear_cliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fr = getFragmentManager().beginTransaction();
+                fr.replace(R.id.fragment_container,new Fragment_crear_cliente()).commit();
+            }
+        });
+        return v;
+
+    }
+
+}
--- app/src/main/java/com/example/jl/bionet/Home.java
+++ app/src/main/java/com/example/jl/bionet/Home.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -31,6 +33,7 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
     }
 
 
@@ -43,8 +46,8 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
         }
     }
 
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
             switch (menuItem.getItemId()) {
                 case R.id.nav_clientes:
                         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
@@ -54,6 +57,21 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
 
             drawer.closeDrawer(GravityCompat.START);
             return true;
+
+    }
+
+    public void replaceFragments(Class fragmentClass) {
+        Fragment fragment = null;
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment)
+                .commit();
+    }
+
 
 }
