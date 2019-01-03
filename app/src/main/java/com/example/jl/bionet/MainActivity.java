f6b7bc032fc8310befb8511760ce318231f73f79
Abrancill0 <38171762+Abrancill0@users.noreply.github.com>
2019-01-03 17:52:52 -0600

    Llamado de api con Parametros y modelo

--- app/src/main/java/com/example/jl/bionet/MainActivity.java
+++ app/src/main/java/com/example/jl/bionet/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.jl.bionet;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.StrictMode;
@@ -11,118 +12,131 @@ import android.view.textclassifier.TextLinks;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.jl.bionet.model.LoginModel;
 
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-
 import java.util.HashMap;
 import java.util.Map;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
+import static com.android.volley.Request.*;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextViewResult;
+
+    ProgressDialog progreso;
+
+    RequestQueue request;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+
+
+
+    private void Login(){
+
+        progreso = new ProgressDialog(this);
+        progreso.setMessage("Iniciando sesion...");
+        progreso.show();
+
         RequestQueue queue = Volley.newRequestQueue(this);  // this = context
 
-       String url = "https://citycenter-rosario.com.ar/usuarios/loginApp";
-        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
-                new Response.Listener<String>()
-                {
-                    @Override
-                    public void onResponse(String response) {
-                        // response
-                        Log.d("Response", response);
-                    }
-                },
+        JSONObject request = new JSONObject();
+        try
+        {
+            request.put("usu_usuario", "abrx23@gmail.com");
+            request.put("usu_contrasena", "123");
+
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        String url = "https://citycenter-rosario.com.ar/usuarios/loginApp";
+
+        JsonObjectRequest postRequest = new JsonObjectRequest(Method.POST, url,request, new Response.Listener<JSONObject>()
+        {
+            @Override
+            public void onResponse(JSONObject response) {
+                progreso.hide();
+
+                LoginModel Resultado = new LoginModel();
+
+                JSONObject Respuesta = null;
+
+                try {
+                    Respuesta = response.getJSONObject("resultado");
+                    
+                    Resultado.setUsuNombre(Respuesta.getString("usu_nombre"));
+                    Resultado.getUsuId(Respuesta.getString("usu_id"));
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+
+                    progreso.hide();
+                }
+
+                     Toast toast1 =
+                             Toast.makeText(getApplicationContext(),
+                                    "Bienvenido " + Resultado.getUsuNombre(), Toast.LENGTH_SHORT);
+
+                     toast1.show();
+            }
+
+        },
                 new Response.ErrorListener()
                 {
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         // error
-                        Log.d("Error", "Error respuesta en Json" + error.getMessage());
+
+                        progreso.hide();
+
+                        Toast toast1 =
+                                Toast.makeText(getApplicationContext(),
+                                       "Error de conexion", Toast.LENGTH_SHORT);
+
+                        toast1.show();
+
                     }
                 }
-        ) {
-            @Override
-            protected Map<String, String> getParams()
-            {
-                Map<String, String>  params = new HashMap<String, String>();
-                params.put("usu_usuario", "abrx23@gmail.com");
-                params.put("usu_contrasena", "123");
+        );
 
-                return params;
-            }
-        };
-
-       queue.add(postRequest);
+        queue.add(postRequest);
 
 
     }
-
-    private void Login(){
-
-    }
-
 
     public void CrearCuenta(View view) {
         Intent intent = new Intent(MainActivity.this, CrearCuentaActivity.class);
         startActivity(intent);
     }
 
-
-
-
     public void IniciarSesion(View view){
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        Login();
     }
-
 
 }
--- app/src/main/java/com/example/jl/bionet/model/LoginModel.java
+++ app/src/main/java/com/example/jl/bionet/model/LoginModel.java
@@ -2,213 +2,203 @@ package com.example.jl.bionet.model;
 
 public class LoginModel {
 
-    public class Resultado {
+        private Integer usuId;
+        private String usuUsuario;
+        private String usuContrasena;
+        private String usuTipoContrasena;
+        private String usuUsuarioBloquedado;
+        private String usuNombre;
+        private String usuApellidos;
+        private String usuEmail;
+        private String usuTelefono;
+        private String usuCelular;
+        private String usuFechaRegistro;
+        private String usuFechaNacimiento;
+        private Integer usuIdTarjetaSocio;
+        private String usuCiudad;
+        private String usuTipoDocumento;
+        private String usuNoDocumento;
+        private String usuContrasenaTemp;
+        private String usuImagen;
+        private Integer usuIdRol;
+        private Integer usuEstatus;
+        private String createdAt;
+        private String updatedAt;
 
-        private int usu_id;
-        private String usu_usuario;
-        private String usu_contrasena;
-        private String usu_tipo_contrasena;
-        private String usu_usuario_bloquedado;
-        private String usu_nombre;
-        private String usu_apellidos;
-        private String usu_email;
-        private String usu_telefono;
-        private String usu_celular ;
-        private String usu_fecha_registro;
-        private String usu_fecha_nacimiento;
-        private int usu_id_tarjeta_socio;
-        private String usu_imagen;
-        private String usu_ciudad;
-        private int usu_id_rol;
-        private int usu_estatus;
-        private String created_at;
-        private String updated_at;
-        private String usu_tipo_documento;
-        private String usu_contrasena_temp;
-        private String usu_no_documento;
-
-
-        public int getUsu_id() {
-            return usu_id;
+        public Integer getUsuId(String usu_id) {
+            return usuId;
         }
 
-        public void setUsu_id(int usu_id) {
-            this.usu_id = usu_id;
+        public void setUsuId(Integer usuId) {
+            this.usuId = usuId;
         }
 
-        public String getUsu_usuario() {
-            return usu_usuario;
+        public String getUsuUsuario() {
+            return usuUsuario;
         }
 
-        public void setUsu_usuario(String usu_usuario) {
-            this.usu_usuario = usu_usuario;
+        public void setUsuUsuario(String usuUsuario) {
+            this.usuUsuario = usuUsuario;
         }
 
-        public String getUsu_contrasena() {
-            return usu_contrasena;
+        public String getUsuContrasena() {
+            return usuContrasena;
         }
 
-        public void setUsu_contrasena(String usu_contrasena) {
-            this.usu_contrasena = usu_contrasena;
+        public void setUsuContrasena(String usuContrasena) {
+            this.usuContrasena = usuContrasena;
         }
 
-        public String getUsu_tipo_contrasena() {
-            return usu_tipo_contrasena;
+        public String getUsuTipoContrasena() {
+            return usuTipoContrasena;
         }
 
-        public void setUsu_tipo_contrasena(String usu_tipo_contrasena) {
-            this.usu_tipo_contrasena = usu_tipo_contrasena;
+        public void setUsuTipoContrasena(String usuTipoContrasena) {
+            this.usuTipoContrasena = usuTipoContrasena;
         }
 
-        public String getUsu_usuario_bloquedado() {
-            return usu_usuario_bloquedado;
+        public String getUsuUsuarioBloquedado() {
+            return usuUsuarioBloquedado;
         }
 
-        public void setUsu_usuario_bloquedado(String usu_usuario_bloquedado) {
-            this.usu_usuario_bloquedado = usu_usuario_bloquedado;
+        public void setUsuUsuarioBloquedado(String usuUsuarioBloquedado) {
+            this.usuUsuarioBloquedado = usuUsuarioBloquedado;
         }
 
-        public String getUsu_nombre() {
-            return usu_nombre;
+        public String getUsuNombre() {
+            return usuNombre;
         }
 
-        public void setUsu_nombre(String usu_nombre) {
-            this.usu_nombre = usu_nombre;
+        public void setUsuNombre(String usuNombre) {
+            this.usuNombre = usuNombre;
         }
 
-        public String getUsu_apellidos() {
-            return usu_apellidos;
+        public String getUsuApellidos() {
+            return usuApellidos;
         }
 
-        public void setUsu_apellidos(String usu_apellidos) {
-            this.usu_apellidos = usu_apellidos;
+        public void setUsuApellidos(String usuApellidos) {
+            this.usuApellidos = usuApellidos;
         }
 
-        public String getUsu_email() {
-            return usu_email;
+        public String getUsuEmail() {
+            return usuEmail;
         }
 
-        public void setUsu_email(String usu_email) {
-            this.usu_email = usu_email;
+        public void setUsuEmail(String usuEmail) {
+            this.usuEmail = usuEmail;
         }
 
-        public String getUsu_telefono() {
-            return usu_telefono;
+        public String getUsuTelefono() {
+            return usuTelefono;
         }
 
-        public void setUsu_telefono(String usu_telefono) {
-            this.usu_telefono = usu_telefono;
+        public void setUsuTelefono(String usuTelefono) {
+            this.usuTelefono = usuTelefono;
         }
 
-        public String getUsu_celular() {
-            return usu_celular;
+        public String getUsuCelular() {
+            return usuCelular;
         }
 
-        public void setUsu_celular(String usu_celular) {
-            this.usu_celular = usu_celular;
+        public void setUsuCelular(String usuCelular) {
+            this.usuCelular = usuCelular;
         }
 
-        public String getUsu_fecha_registro() {
-            return usu_fecha_registro;
+        public String getUsuFechaRegistro() {
+            return usuFechaRegistro;
         }
 
-        public void setUsu_fecha_registro(String usu_fecha_registro) {
-            this.usu_fecha_registro = usu_fecha_registro;
+        public void setUsuFechaRegistro(String usuFechaRegistro) {
+            this.usuFechaRegistro = usuFechaRegistro;
         }
 
-        public String getUsu_fecha_nacimiento() {
-            return usu_fecha_nacimiento;
+        public String getUsuFechaNacimiento() {
+            return usuFechaNacimiento;
         }
 
-        public void setUsu_fecha_nacimiento(String usu_fecha_nacimiento) {
-            this.usu_fecha_nacimiento = usu_fecha_nacimiento;
+        public void setUsuFechaNacimiento(String usuFechaNacimiento) {
+            this.usuFechaNacimiento = usuFechaNacimiento;
         }
 
-        public int getUsu_id_tarjeta_socio() {
-            return usu_id_tarjeta_socio;
+        public Integer getUsuIdTarjetaSocio() {
+            return usuIdTarjetaSocio;
         }
 
-        public void setUsu_id_tarjeta_socio(int usu_id_tarjeta_socio) {
-            this.usu_id_tarjeta_socio = usu_id_tarjeta_socio;
+        public void setUsuIdTarjetaSocio(Integer usuIdTarjetaSocio) {
+            this.usuIdTarjetaSocio = usuIdTarjetaSocio;
         }
 
-        public String getUsu_imagen() {
-            return usu_imagen;
+        public String getUsuCiudad() {
+            return usuCiudad;
         }
 
-        public void setUsu_imagen(String usu_imagen) {
-            this.usu_imagen = usu_imagen;
+        public void setUsuCiudad(String usuCiudad) {
+            this.usuCiudad = usuCiudad;
         }
 
-        public String getUsu_ciudad() {
-            return usu_ciudad;
+        public String getUsuTipoDocumento() {
+            return usuTipoDocumento;
         }
 
-        public void setUsu_ciudad(String usu_ciudad) {
-            this.usu_ciudad = usu_ciudad;
+        public void setUsuTipoDocumento(String usuTipoDocumento) {
+            this.usuTipoDocumento = usuTipoDocumento;
         }
 
-        public int getUsu_id_rol() {
-            return usu_id_rol;
+        public String getUsuNoDocumento() {
+            return usuNoDocumento;
         }
 
-        public void setUsu_id_rol(int usu_id_rol) {
-            this.usu_id_rol = usu_id_rol;
+        public void setUsuNoDocumento(String usuNoDocumento) {
+            this.usuNoDocumento = usuNoDocumento;
         }
 
-        public int getUsu_estatus() {
-            return usu_estatus;
+        public String getUsuContrasenaTemp() {
+            return usuContrasenaTemp;
         }
 
-        public void setUsu_estatus(int usu_estatus) {
-            this.usu_estatus = usu_estatus;
+        public void setUsuContrasenaTemp(String usuContrasenaTemp) {
+            this.usuContrasenaTemp = usuContrasenaTemp;
         }
 
-        public String getCreated_at() {
-            return created_at;
+        public String getUsuImagen() {
+            return usuImagen;
         }
 
-        public void setCreated_at(String created_at) {
-            this.created_at = created_at;
+        public void setUsuImagen(String usuImagen) {
+            this.usuImagen = usuImagen;
         }
 
-        public String getUpdated_at() {
-            return updated_at;
+        public Integer getUsuIdRol() {
+            return usuIdRol;
         }
 
-        public void setUpdated_at(String updated_at) {
-            this.updated_at = updated_at;
+        public void setUsuIdRol(Integer usuIdRol) {
+            this.usuIdRol = usuIdRol;
         }
 
-        public String getUsu_tipo_documento() {
-            return usu_tipo_documento;
+        public Integer getUsuEstatus() {
+            return usuEstatus;
         }
 
-        public void setUsu_tipo_documento(String usu_tipo_documento) {
-            this.usu_tipo_documento = usu_tipo_documento;
+        public void setUsuEstatus(Integer usuEstatus) {
+            this.usuEstatus = usuEstatus;
         }
 
-        public String getUsu_contrasena_temp() {
-            return usu_contrasena_temp;
+        public String getCreatedAt() {
+            return createdAt;
         }
 
-        public void setUsu_contrasena_temp(String usu_contrasena_temp) {
-            this.usu_contrasena_temp = usu_contrasena_temp;
+        public void setCreatedAt(String createdAt) {
+            this.createdAt = createdAt;
         }
 
-        public String getUsu_no_documento() {
-            return usu_no_documento;
+        public String getUpdatedAt() {
+            return updatedAt;
         }
 
-        public void setUsu_no_documento(String usu_no_documento) {
-            this.usu_no_documento = usu_no_documento;
+        public void setUpdatedAt(String updatedAt) {
+            this.updatedAt = updatedAt;
         }
+
     }
-
-    public class LoginReturn {
-        private int estatus;
-        private String mensaje;
-        private Resultado resultado;
-    }
-
-}
