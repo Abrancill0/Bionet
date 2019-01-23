0c58381dda9cef618dea6e54043500b55972c5a6
José Luis <joseluismartinezdominguez16@gmail.com>
2019-01-23 15:19:09 -0600

    cambiosJL
    Cambios que no subio yo JL

--- app/src/main/java/com/example/jl/bionet/Home.java
+++ app/src/main/java/com/example/jl/bionet/Home.java
@@ -1,24 +1,55 @@
 package com.example.jl.bionet;
 
+import android.Manifest;
 import android.app.Dialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 
-public class Home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+public class Home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, Fragment_pop_up_ProfilePhoto.OnPhotoSelectedListener {
     private DrawerLayout drawer;
 
     Dialog cerrar;
+    private static final int REQUEST_CODE = 2312424;
+    private Bitmap mSelectedBitmap;
+    private Uri mSelectedUri;
+    ImageLoader imageLoader = ImageLoader.getInstance();
+
+    @Override
+    public void getImageBitmap(Bitmap bitmap) {
+        ImageView SelectPhoto = (ImageView) findViewById(R.id.foto_perfil);
+        SelectPhoto.setImageBitmap(bitmap);
+        mSelectedUri = null;
+        mSelectedBitmap = bitmap;
+
+    }
+
+    @Override
+    public void getImagePath(Uri imagePath) {
+        ImageView SelectPhoto = (ImageView) findViewById(R.id.foto_perfil);
+        imageLoader.displayImage(imagePath.toString(),SelectPhoto);
+        mSelectedBitmap = null;
+        mSelectedUri = imagePath;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +68,44 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+        View headView = navigationView.getHeaderView(0);
+        ImageView imgProfile = headView.findViewById(R.id.foto_perfil);
+        imgProfile.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                verifyPermissions();
+                FragmentManager fm = getSupportFragmentManager();
+                Fragment_pop_up_ProfilePhoto myDialogFragment = new Fragment_pop_up_ProfilePhoto();
+                myDialogFragment.show(fm, "photo_dialog_fragment");
+            }
+        });
 
+
+    }
+    private void verifyPermissions(){
+
+        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA};
+
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                permissions[0])== PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                permissions[1])== PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                permissions[2])== PackageManager.PERMISSION_GRANTED) {
+
+        }else{
+            ActivityCompat.requestPermissions(Home.this,
+                    permissions,
+                    REQUEST_CODE);
+
+        }
+
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        verifyPermissions();
     }
 
 
@@ -54,8 +122,8 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
             switch (menuItem.getItemId()) {
                 case R.id.nav_clientes:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                                new Fragment_clientes()).commit();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            new Fragment_clientes()).commit();
                     break;
 
                 case R.id.nav_inventario:
@@ -64,14 +132,32 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
                     break;
 
                 case R.id.nav_cerrar_sesion:
-                        cerrar.setContentView(R.layout.pop_up_cerrarsesion);
-                        cerrar.show();
+                    cerrar.setContentView(R.layout.pop_up_cerrarsesion);
+                    cerrar.show();
                     break;
             }
 
             drawer.closeDrawer(GravityCompat.START);
             return true;
 
+    }
+
+
+
+
+    public void clientes(View view){
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new Fragment_clientes()).commit();
+    }
+
+    public void inventario(View view){
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new Fragment_inventarios()).commit();
+    }
+
+    public void cerrar_sesion(View view){
+        cerrar.setContentView(R.layout.pop_up_cerrarsesion);
+        cerrar.show();
     }
 
     public void replaceFragments(Class fragmentClass) {
