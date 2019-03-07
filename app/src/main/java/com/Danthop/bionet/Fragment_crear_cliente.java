4219a28cb1316ed30b194a0b9ff95dc5d3bfba3d
José Luis <joseluismartinezdominguez16@gmail.com>
2019-03-07 11:08:56 -0600

    Tabla con información vacía

--- app/src/main/java/com/Danthop/bionet/Fragment_crear_cliente.java
+++ app/src/main/java/com/Danthop/bionet/Fragment_crear_cliente.java
@@ -183,7 +183,7 @@ public class Fragment_crear_cliente extends DialogFragment {
             @Override
             public void onClick(View v) {
 
-                Verificar_isChecked(Mismo_email_personal,Mismo_direccion_personal);
+                GuardarCliente();
 
             }
         });
@@ -342,14 +342,6 @@ public class Fragment_crear_cliente extends DialogFragment {
 
         VolleySingleton.getInstanciaVolley(getContext()).addToRequestQueue(postRequest);
 
-    }
-
-    public void Verificar_isChecked(CheckBox Mismo_email, CheckBox Misma_direccion){
-
-
-
-
-            return;
     }
 
     private void LoadSpinnerEstado(){
