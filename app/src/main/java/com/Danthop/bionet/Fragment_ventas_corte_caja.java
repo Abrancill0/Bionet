c28e72d8118590cb0992c580364f4243f1733313
José Luis <joseluismartinezdominguez16@gmail.com>
2019-04-01 13:55:43 -0600

    Cambios ventas
    Se añadió el crear clientes en la parte de agregar cliente en ventas

--- app/src/main/java/com/Danthop/bionet/Fragment_ventas_corte_caja.java
+++ app/src/main/java/com/Danthop/bionet/Fragment_ventas_corte_caja.java
@@ -3,15 +3,19 @@ package com.Danthop.bionet;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Fragment_ventas_corte_caja extends Fragment {
+
+    Button pestania_ventas;
 
 
     public Fragment_ventas_corte_caja() {
@@ -23,6 +27,14 @@ public class Fragment_ventas_corte_caja extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_ventas_corte_caja_listado,container, false);
+        pestania_ventas = v.findViewById(R.id.Ventas_btn);
+        pestania_ventas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fr = getFragmentManager().beginTransaction();
+                fr.replace(R.id.fragment_container,new Fragment_Ventas()).commit();
+            }
+        });
         return v;
     }
 
