1862ca8b580882fd818eea9078520a04b1549d33
GuadalupeMendozaCastillo <48536521+GuadalupeMendozaCastillo@users.noreply.github.com>
2019-05-01 10:49:50 -0500

    Cambios Lupita
    En dashboard ya es posible ver -Top de articulos mas vendidos
    -Top pocas existencias
    -Top clientes frecuentes

--- app/src/main/java/com/Danthop/bionet/model/ClienteFrecuenteModel.java
+++ app/src/main/java/com/Danthop/bionet/model/ClienteFrecuenteModel.java
@@ -1,16 +1,39 @@
 package com.Danthop.bionet.model;
 
+import java.util.function.Function;
+
 public class ClienteFrecuenteModel {
 
     private String nombre_cliente;
+    private String NumTicket;
+    private String UUIDTicketCliente;
+    private String tar_nombre_articulo;
+    private String tar_id_articulo;
 
-    public ClienteFrecuenteModel(String nombre_cliente)
+    public ClienteFrecuenteModel(String nombre_cliente, String NumTicket, String UUIDTicketCliente, String tar_nombre_articulo, String tar_id_articulo)
     {
         this.nombre_cliente = nombre_cliente;
+        this.NumTicket = NumTicket;
+        this.UUIDTicketCliente = UUIDTicketCliente;
+        this.tar_nombre_articulo = tar_nombre_articulo;
+        this.tar_id_articulo = tar_id_articulo;
+
     }
 
     public String getnombre_cliente() {return nombre_cliente; }
     public void setnombre_cliente(String nombre_cliente) { this.nombre_cliente = nombre_cliente; }
+
+    public String getNumTicket() {return NumTicket; }
+    public void setNumTicket(String NumTicket) { this.NumTicket = NumTicket; }
+
+    public String getUUIDTicketCliente() {return UUIDTicketCliente; }
+    public void setUUIDTicketCliente(String UUIDTicketCliente) { this.UUIDTicketCliente = UUIDTicketCliente; }
+
+    public String gettar_nombre_articulo() {return tar_nombre_articulo; }
+    public void settar_nombre_articulo(String tar_nombre_articulo) { this.tar_nombre_articulo = tar_nombre_articulo; }
+
+    public String gettar_id_articulo() {return tar_id_articulo; }
+    public void settar_id_articulo(String tar_id_articulo) { this.tar_id_articulo = tar_id_articulo; }
 
 
 }
